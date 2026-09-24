42290f944d6735062345f6420095913b112ab904
Harley D. Eades III <harley.eades@pm.me>
2026-09-24 19:45:33 -0400

    Years are now included when filtering activities.

--- themes/nab-dark/assets/js/main.js
+++ themes/nab-dark/assets/js/main.js
@@ -1,22 +1,29 @@
 const filter = document.querySelector("#filter-input");
-const items = document.querySelectorAll("#feed > li");
+const items = document.querySelectorAll(".academic-activity-feed-item");
+const yearHeadings = document.querySelectorAll(".academic-activity-feed-item-year");
 
 filter.addEventListener("input", () => {
   const query = filter.value.trim().toLowerCase();
+
+  yearHeadings.forEach((yearHeading) => {        
+        yearHeading.style.display = "none"
+      });
 
   items.forEach((item) => {
     const title = item.getAttribute("data-title")?.toLowerCase() ?? "";
     const tags = item.getAttribute("data-tags")?.toLowerCase() ?? "";
     const year = item.getAttribute("data-year")?.toLowerCase() ?? "";
 
-    if (title.includes(query)) {
-        console.log(title);
-    }
     const matches = 
-      title.includes(query) ||
+      (title.includes(query) ||
       tags.includes(query)  ||
-      year.includes(query);
+      year.includes(query));
     
-    item.style.display = matches ? "" : "none";    
+      if (matches) {
+        item.style.display = "";
+        document.querySelector(`#year-${year}`).style.display = "";
+      } else {
+        item.style.display = "none";
+      }
   });
 });
